1abf0bddf97a904bc82ec6e004f16a31befdc473
thicczucc <therealrp231@gmail.com>
2019-11-01 16:58:54 -0700

    even even even even more updates

--- event.js
+++ event.js
@@ -2,7 +2,7 @@ function getCredentials(){
   var username = document.getElementById("loginUsername").value;
   var password = document.getElementById("loginPassword").value;
   const userAction = async ()=> {
-    const response = await fetch('http://127.0.0.1:5000/sendemail'), {
+    const response = await fetch('http://127.0.0.1:5000/sendemail', {
       method: 'POST',
       body: {
         "username": username,
